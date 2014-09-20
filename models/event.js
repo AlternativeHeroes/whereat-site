fa306005ed5af3d0055e61e42f98abf193032460
Andrew Dai <theman@andrewdai.co>
2014-09-20 10:17:13 -0400

    added event model

---
+++ models/event.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+
+var eventSchema = mongoose.Schema({
+  name: String,
+  description: String,
+  time: Date,
+  where: String,
+  host: String, // user id
+  pictures: Array, // array of picture urls
+  attendees: Array, // array of user ids
+  ratings: Array // array of ...?
+});
+
+module.exports = mongoose.model('Event', eventSchema);;
