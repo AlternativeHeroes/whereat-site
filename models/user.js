fd504965101207be0f17b70ed01917b45f9910b5
Nick Kalteux <kalteuxn@gatech.edu>
2014-09-20 10:38:33 -0400

    added phone number field to user

--- models/user.js
+++ models/user.js
@@ -3,15 +3,23 @@ var mongoose = require('mongoose');
 function toLower (v) {
   return v.toLowerCase();
 }
-
+function validatePhone (v) {
+  var targ=v.replace(/[^\d]/g,''); // remove all non-digits
+  if(targ && targ.length===10) {
+  // targ is a valid phone number
+    return v;
+  }
+  return;
+}
 var userSchema = mongoose.Schema({
 		name: { type: String, required: true },
     bio: String,
     facebook: String,
     twitter: String,
     email: { type: String, set: toLower },
+    phoneNumber: {type: String, set: validatePhone, default: "9782061324" },
     picture:  String,
-    currentEvent: String,
+    currentEvent: String
 	})
 
 module.exports = mongoose.model("User", userSchema);
